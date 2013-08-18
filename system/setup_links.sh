59ed7e33b28b8ba3df733e8800f2d3b6a0b2af24
Srinath Krishna Ananthakrishnan <srinath.krishna@gmail.com>
2013-08-18 00:38:46 -0700

    added some system level scripts

---
+++ system/setup_links.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+ln -s .dotfiles/.config ~/.config
+ln -s .dotfiles/.vim ~/.vim
+ln -s .dotfiles/bin ~/bin
+
+ln -s .dotfiles/.bash_profile ~/.bash_profile
+ln -s .dotfiles/.bashrc ~/.bashrc
+
+if [[ `uname` == 'Linux' ]]; then
+	ln -s .dotfiles/.gtkrc-2.0 ~/.gtkrc-2.0
+	ln -s .dotfiles/.gtkrc.mine ~/.gtkrc.mine
+fi
+
+ln -s .dotfiles/.myshrc ~/.myshrc
+ln -s .dotfiles/.profile ~/.profile
+ln -s .dotfiles/.screenrc ~/.screenrc
+ln -s .dotfiles/.tmux.conf ~/.tmux.conf
+ln -s .dotfiles/.vimrc ~/.vimrc
+
+if [[ `uname` == 'Linux' ]]; then
+	ln -s .dotfiles/.xinitrc ~/.xinitrc
+	ln -s .dotfiles/.xmodmap ~/.xmodmap
+fi
+
+ln -s .dotfiles/.zprofile ~/.zprofile
+ln -s .dotfiles/.zshrc ~/.zshrc
