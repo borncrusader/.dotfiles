ccfbdb87f319d37f2d6ceb01f3c5493c0c6fe98c
Srinath Krishna Ananthakrishnan <sananthakrishnan@riverbed.com>
2013-10-03 06:26:23 +0000

    updated (finally!) awesome rcs and gtkrc. also, added dual_setup

--- .config/awesome/theme.lua
+++ .config/awesome/theme.lua
@@ -22,6 +22,10 @@ theme.border_normal = "#000000"
 theme.border_focus  = "#535d6c"
 theme.border_marked = "#91231c"
 
+-- {{{ Variable definitions
+local home = os.getenv("HOME")
+-- }}}
+
 -- There are other variable sets
 -- overriding the default one when
 -- defined, the sets are:
@@ -72,7 +76,7 @@ theme.titlebar_maximized_button_focus_inactive  = "/usr/share/awesome/themes/def
 theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = "/home/sananth5/.config/awesome/cosmos.jpg"
+theme.wallpaper = home+"/.config/awesome/cosmos.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
@@ -92,7 +96,7 @@ theme.awesome_icon = "/usr/share/awesome/icons/awesome16.png"
 
 -- Define the icon theme for application icons. If not set then the icons 
 -- from /usr/share/icons and /usr/share/icons/hicolor will be used.
-theme.icon_theme = "/usr/share/icons/Faenza/index.theme"
+--theme.icon_theme = "/usr/share/icons/Faenza/index.theme"
 
 return theme
 -- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
