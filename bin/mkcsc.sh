27ea8750daa361e997ab670e81f053dc32763ec1
Srinath Krishna Ananthakrishnan <srinath.krishna@gmail.com>
2013-10-13 15:40:27 -0700

    adding a script for generating cscope databases from files

---
+++ bin/mkcsc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+f="cscope.files"
+
+if [ ! -z $1 ]; then
+	if [ $1 == '-h' ]; then
+		echo "usage: $0 {temp-file}"
+		echo "  default temp-file is $f"
+		exit
+	fi
+
+	f=$1
+fi
+
+echo "generating list of files to $f"
+if [ `uname` == "Darwin" ]; then
+	find -E . -type f -regex '.*/.*\.([ch](pp|xx)?|cc|C)' > $f
+elif [ `uname` == "Linux" ]; then
+	find . -regextype posix-extended -type f -regex '.*/.*\.([ch](pp|xx)?|cc|C)' > $f
+fi
+
+echo "generating cscope database"
+if [ -e $f ]; then
+	cscope -bqi $f
+fi
