bd4eccd4588cf87bdbdf57c5c8ed044a8e4687a5
Srinath Ananthakrishnan <srinath.krishna@gmail.com>
2024-11-03 22:08:58 -0800

    new nvim

--- .config/nvim-kick/lua/custom/plugins/init.lua
+++ .config/nvim-kick/lua/custom/plugins/init.lua
@@ -1,1 +1,10 @@
-return {}
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+    {
+        "sourcegraph/sg.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    },
+}
