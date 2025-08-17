be092335d94d87ca07d7c1906ba0ba33531f7975
Srinath Ananthakrishnan <srinath.krishna@gmail.com>
2025-08-17 16:44:49 -0700

    Some more plugins shenanigans

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -984,7 +984,7 @@ require('lazy').setup({
 })
 
 -- Sourcegraph configuration
-require('sg').setup {}
+-- require('sg').setup {}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
